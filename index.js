6dc540f27249ef3f208bf82c92a643f5132c4031
BarberoMiguel <migomab@gmail.com>
2023-09-24 22:20:20 +0200

    ejercicios resueltos

---
+++ index.js
@@ -0,0 +1,52 @@
+let variableSinValor = undefined;
+let booleano1 = true;
+let booleano2 = false;
+const PI = 3.14;
+const TAU = 2*PI;
+var booleanoAnd = booleano1 == booleano2;
+var booleanoNot = !booleano1;
+var booleanoMix0 = (booleano1 || booleano2) && (booleano1 || (!booleano1 && !booleano2));
+var incrementarDesp = 2;
+var resultadoDesp = incrementarDesp++;
+var incrementarAntes = 2;
+var resultadoAntes = ++incrementarAntes;
+for (var contarHasta10_2 = 0; contarHasta10_2 < 10; contarHasta10_2++) {
+}
+var postI = 0;
+var postJ = 0;
+for (i = 0; i < 11; i++) {
+    postI = postI + postJ++;
+}
+var sumaPares = 0;
+for (i = 0; i < 10; i++) {
+    if (i % 2 == 0) {
+        sumaPares = sumaPares + i;
+    }
+}
+let variableValorNumerico = 25;
+const MiNombre = "Miguel";
+const MiNumeroFav = 2007;
+var booleanoOr = booleano1 || booleano2;
+var booleanoMix1 = (booleano1 && (TAU/2 == PI)) || (variableValorNumerico >= MiNumeroFav);
+var seisNoEsNueve = (6 !== 9);
+var booleanoMix2 = (variableValorNumerico > 0) || (variableValorNumerico < -(MiNumeroFav*TAU));
+var valorSuma = variableValorNumerico + MiNumeroFav;
+var valorResta = MiNumeroFav - variableValorNumerico;
+var valorMultiplicacion = MiNumeroFav*variableValorNumerico;
+var valorDivision = MiNumeroFav/3;
+var contarHasta10 = 0;
+while (contarHasta10 !== 10) {
+    contarHasta10 = contarHasta10 + 1;
+}
+var preJ = 0;
+var preI = 0;
+for (i = 0; i < 11; i++) {
+    preI = preI + ++preJ;
+}
+var sumaImpares = 0;
+for (i = 0; i < 10; i++) {
+    if (i % 2 == 1) {
+        sumaImpares = sumaImpares + i;
+    }
+}
+    
